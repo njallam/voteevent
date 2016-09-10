e4cf2efa27c571b883ccc5f85e0e0fca9080a499
Nicholas Allam <njallam@outlook.com>
2016-09-10 20:43:42 +0100

    Redirect invalid paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :events, only: [:index, :show]
   post '/vote/:id', to: 'events#vote', as: 'vote'
   root to: 'events#index'
+  get '*path' => redirect('/')
 end
