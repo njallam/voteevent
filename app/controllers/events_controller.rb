a893fa2f222029ad7df78b384d7cd88c8b2a9be6
Nicholas Allam <njallam@outlook.com>
2016-09-10 20:43:07 +0100

    Voting functionality

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,26 @@
+class EventsController < ApplicationController
+  def index
+  end
+
+  before_action only: [:show, :vote] do |controller|
+    @event = Event.find(params[:id])
+    @attendance = Attendance.find_by(event: @event, person: @person)
+    @voted = @attendance.voted
+    @occurred = @event.date < DateTime.now
+  end
+
+  def show
+  end
+
+  def vote
+    if not @voted and @occurred then
+      score = params[:score].to_i
+      if score > 0 and score <= 10 then
+        @event.score += score
+        @attendance.voted = true
+        @event.save
+        @attendance.save
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events, only: [:index, :show]
+  post '/vote/:id', to: 'events#vote', as: 'vote'
+  root to: 'events#index'
 end
